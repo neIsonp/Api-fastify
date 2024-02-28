cf9c5b783a711d0cd3e0df26dc8c082eaffffc0d
neIsonp <nelsonponte37@gmail.com>
2024-02-28 16:19:44 -0100

    create hashPassword and verifyPassword method

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,6 +1,22 @@
 import { FastifyReply, FastifyRequest } from "fastify";
+import { CreateUserInput } from "./user.schema";
+import { createUser } from "./user.service";
 
 export async function registerUserHandler(
-  req: FastifyRequest,
+  req: FastifyRequest<{
+    Body: CreateUserInput;
+  }>,
   res: FastifyReply
-) {}
+) {
+  const body = req.body;
+
+  try {
+    const user = await createUser(body);
+
+    return res.code(201).send(user);
+  } catch (error) {
+    console.log(error);
+
+    return res.code(500).send(error);
+  }
+}
